8cd3d7b1451cd972c900275ff250c60e6b9a1f76
Atikingi <n.igumentsev@gmail.com>
2022-08-30 12:53:30 +0300

    prepare

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
